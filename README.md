81f7b1c031bdd043e84bd72c261347c1c02cd450
JazC <jchan7713@gmail.com>
2023-11-11 23:01:34 -0800

    Add Kafka producer configurations, add Kafka notes in README

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Kafka-Tutorial-SpringBoot-Microservices
+# Kafka Tutorial - SpringBoot Microservices
 Following tutorial at: www.youtube.com/watch?v=SqVfCyfCJqw&amp;t
+
+# Notes
+
+- Each message in a Kafka topic is a key-value (data) pair.
+  - **Key** (optional): used for partitioning messages across Kafka topics
+  - **Value**: main payload of the message
+
+- Before data (key and value) is stored in Kafka topics, the data needs to be serialized (converted into a sequence of bytes) (KafkaProducerConfig.java).
+- This is because Kafka is a distributed system, so data needs to be sent in a form that can be transmitted across the network.
+- Producers produce serialized data.
+- Consumers deserialize the data (key and value) with a corresponding deserializer in order to interpret the bytes.
--- src/main/java/com/jc/kafkatutorial/kafka/config/KafkaProducerConfig.java
+++ src/main/java/com/jc/kafkatutorial/kafka/config/KafkaProducerConfig.java
@@ -1,7 +1,16 @@
 package com.jc.kafkatutorial.kafka.config;
 
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class KafkaProducerConfig {
@@ -9,4 +18,27 @@ public class KafkaProducerConfig {
     // variable that will hold the bootstrap server url defined in application.properties
     @Value("{spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
+
+    // Configuration to pass to a producer factory
+    // Configuring port to use for establishing the initial connection to the Kafka cluster
+    // Configuring Kafka so that keys and values will be serialized into the StringSerializer.class
+    public Map<String, Object> producerConfig() {  // For producing Kafka producers. All producers will produce keys and values that are serialized into strings.
+        Map<String, Object> property = new HashMap<>();
+        property.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        property.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        property.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return property;
+    }
+
+    // Defining the producer factory, which is responsible for creating producer instances
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfig());
+    }
+
+    // A way to send messages
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String, String> producerFactory) {
+        return new KafkaTemplate<>(producerFactory());  // Dependency injection
+    }
 }
